72dc5249836df0bfd7c5cc1c8c98d65da76b17b6
newby101 <ishimwekevin45@gmail.com>
2023-10-20 11:54:27 -0400

    insp1.0

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,28 @@
-import hero1 from "../assets/hero1.png";
+import hero1 from "../assets/hero1.jpg";
 import warranty from "../assets/waranty.png";
-import hero2 from "../assets/hero2.png";
+import hero2 from "../assets/hero2.jpg";
 import SubHero1 from "./SubHero1";
+import SubHero2 from "../assets/subhero2.jpg";
 import { TestimonialsHead, TestimonialsBody } from "./Testimonials";
 import { NavLink } from "react-router-dom";
 function Hero() {
+  const features = [
+    "✓ Warranty 	Included",
+    "✓ Quick Service",
+    "✓ All Major Brands",
+    "✓ Quality Repair 	",
+    "✓ Service Fee 	Waived with Repairs",
+    "✓ Repair Low Cost",
+    "✓ Experience",
+  ];
+
+  const features2 = [
+    "https://www.capitalappliancerepairboston.com/wp-content/uploads/2022/05/capital-appliance-repair-google-reviews.png",
+    "https://www.capitalappliancerepairboston.com/wp-content/uploads/2022/05/appliance-repair-satisfaction-guaranteed.png",
+    "https://www.capitalappliancerepairboston.com/wp-content/uploads/2022/05/capital-appliance-repair-trust-pilot.png",
+    "https://www.capitalappliancerepairboston.com/wp-content/uploads/2022/05/capital-appliance-repair-trustedpros.png",
+  ];
+
   return (
     <div className="mx-2 md:mx-[7%] ">
       <div className=" flex flex-wrap md:flex-nowrap">
@@ -27,20 +45,31 @@ function Hero() {
             <div className="mt-5">
               <NavLink
                 to={"request-quote"}
-                className="py-2 px-8 bg-primary text-white rounded-md hover:scale-105 transition-all"
+                className="py-2.5 border-2 border-primary px-8 bg-primary text-white rounded-md hover:scale-105 transition-all text-xl hover:bg-primaryDark "
               >
                 BOOK A REPAIR
               </NavLink>
               <a
                 href="tel:(978)733-0701"
-                className="py-1.5 px-8 ml-2 bg-none text-primary rounded-md border-primary border-2 hover:scale-105 transition-all"
+                className="text-xl py-2.5 px-12 ml-2 bg-none text-primary rounded-md border-primary border-2 hover:scale-150 transition-all hover:bg-primary hover:text-white"
               >
                 CALL US
               </a>
             </div>
           </div>
           <div className="flex flex-col">
-            <img src={hero2} alt="" className="mt-10 mr-4" />
+            <div className="flex">
+              <img
+                src={hero2}
+                alt=""
+                className="mt-10 h-[25em] w-[80%] rounded-2xl shadow-sm rounded-r-none"
+              />
+              <img
+                src={SubHero2}
+                alt=""
+                className="mt-10 mr-4 h-[25em] w-[80%] rounded-xl shadow-sm rounded-l-none"
+              />
+            </div>
             <div className="hidden lg:grid">
               <TestimonialsHead />
             </div>
@@ -49,16 +78,23 @@ function Hero() {
 
         <div className="md:flex flex-col md:ml-12 md:w-[50%]">
           <div className="hidden md:flex">
-            <img src={hero1} alt="" className="h-[70vh] mt-3" />
-            <div className="flex flex-col">
-              <img src={warranty} alt="" />
-              {/* <div>
-            <h1>
-              50%
-              <span>OFF</span>
+            <img
+              src={hero1}
+              alt=""
+              className="h-[70vh] mt-3 rounded-lg object-contain"
+            />
 
-            </h1>
-          </div> */}
+            <div className="flex flex-col ml-4  justify-center">
+              {/* <img src={warranty} alt="" /> */}
+              {features2.map((feature) => {
+                return (
+                  <img
+                  src={feature}
+                    className="mt-3 font-semibold text-primaryDark"
+                    key={feature}
+                  />
+                );
+              })}
             </div>
           </div>
           <div>
@@ -77,3 +113,14 @@ function Hero() {
 }
 
 export default Hero;
+
+// <div className="flex flex-col ml-4  justify-center">
+//   {/* <img src={warranty} alt="" /> */}
+//   {features.map((feature) => {
+//     return (
+//       <h1 className="mt-3 font-semibold text-primaryDark" key={feature}>
+//         {feature}
+//       </h1>
+//     );
+//   })}
+// </div>
